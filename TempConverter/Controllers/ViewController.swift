32285db2603caf189d9716b8ad475267ba9af49a
Patrīcija Vainovska <vainovska.patricija@gmail.com>
2023-04-16 06:46:38 +0300

    Almost done homework

--- TempConverter/Controllers/ViewController.swift
+++ TempConverter/Controllers/ViewController.swift
@@ -11,7 +11,6 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var celsiusLabel: UILabel!
     @IBOutlet weak var resultLabel: UILabel!
-    
     @IBOutlet weak var tempSlider: UISlider!{
         didSet{
             tempSlider.maximumValue = 100
@@ -19,40 +18,44 @@ class ViewController: UIViewController {
             tempSlider.value = 0
         }
     }
-    
+    @IBOutlet weak var segmentSwitch: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        resultLabel.text = "32 Fº"
+        updateLabelValues()
     }
 
     @IBAction func sliderValueChanged(_ sender: Any) {
-        // print("tempSlider:", tempSlider.value)
-        updateResultLabelValue(value: tempSlider.value)
+        updateLabelValues()
     }
     
     @IBAction func segmentValueChanged(_ sender: Any) {
-        updateResultLabelValue(value: tempSlider.value)
+        updateLabelValues()
     }
     
-    func updateResultLabelValue(value: Float) {
-        print(value)
+    func updateLabelValues() {
+        var value = tempSlider.value
         
         let celsiusTemp = Int(value)
-        resultLabel.text = "\(celsiusTemp) Cº" // Update celsisu label instead
+        celsiusLabel.text = "\(celsiusTemp) Cº"
         
-        var convertedTempString = ""
-        // TODO: Use switch case, run convertTempFrom()
-        // switch case, convertedTempString = converTempFromcelsius:celsisusTemp
+        let conversionChart = convertTempFrom(celsius: celsiusTemp)
         
-        /// TODO: Dark light mode again. corner radius, create same info darklight mode
-        /// make both of them like modals (like settings), but the same logic
-        /// constraints
+        let segmentSwitchIndex = segmentSwitch.selectedSegmentIndex
+        if segmentSwitchIndex == 0 {
+            resultLabel.text = "\(conversionChart.fahrenheit) Fº"
+        } else {
+            resultLabel.text = "\(conversionChart.kelvin) Kº"
+        }
+        
+        // TODO: Dark light mode again. corner radius, create same info darklight mode
+        // make both of them like modals (like settings), but the same logic
     }
     
-    func convertTempFrom(celsius: Int) -> (fahrenheit: Double, kelvin: Double) {
-        // TODO: Implement calculation logic
-        return (0,0)
+    func convertTempFrom(celsius: Int) -> (fahrenheit: Int, kelvin: Int) {
+        let temparatureInDegreesFahrenheit = Int(1.8 * Double(celsius) + 32)
+        let temperatureInKelvins = celsius + 273
+        return (temparatureInDegreesFahrenheit, temperatureInKelvins)
     }
 }
 
